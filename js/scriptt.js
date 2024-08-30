d2891e1645cf1e7da847aae76737b7e11ff6ccc8
Blacket-dev <forbetterm@gmail.com>
2024-08-30 23:43:31 +0300

    Update scriptt.js

--- js/scriptt.js
+++ js/scriptt.js
@@ -8,6 +8,14 @@ const products = [
             { image: './image/dt2.jpeg' },
             { image: './image/dt3.jpg' },
             { image: './image/dt4.jpg' },
+             { image: './image/dt4.jpg' },
+            { image: './image/dt5.jpg' },
+            { image: './image/dt6.jpg' },
+            { image: './image/dt7.jpg' },
+            { image: './image/dt8.jpg' },
+            { image: './image/dt9.jpg' },
+            { image: './image/dt10.jpg' },
+            { image: './image/dt11.jpg' },
    
         ]
     },
@@ -19,14 +27,7 @@ const products = [
             { image: './image/so2.jpg' },
             { image: './image/so19.jpg' },
             { image: './image/so4.jpg' },
-            { image: './image/so5.jpg' },
-            { image: './image/so6.jpg' },
-            { image: './image/so7.jpg' },
-            { image: './image/so8.jpg' },
-            { image: './image/so9.jpg' },
-            { image: './image/so10.jpg' },
-            { image: './image/so11.jpg' },
-            { image: './image/so1.jpg' },
+          
         ]
     },
     {
